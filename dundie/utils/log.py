8cbb8a87c931b4a27cea477e15a7f05686a9b68f
Diego Neves <diego@diegoneves.eti.br>
2023-03-10 13:40:46 -0300

    Organized tests

---
+++ dundie/utils/log.py
@@ -0,0 +1,24 @@
+import os
+import logging
+from logging import handlers
+
+LOG_LEVEL = os.getenv("LOG_LEVEL", "WARNING").upper()
+log = logging.Logger("dundie", LOG_LEVEL)
+
+
+def get_logger(logfile="dundie.log"):
+    """Returns a configured log"""    
+    fh = handlers.RotatingFileHandler(
+        "meulog.log",
+        maxBytes=300,
+        backupCount=10,
+    )
+    fh.setLevel(LOG_LEVEL)
+    fmt = logging.Formatter(
+        '%(asctime)s %(name)s %(levelname)s '
+        'l:%(lineno)d f:%(filename)s: %(message)s'
+    )
+
+    fh.setFormatter(fmt)
+    log.addHandler(fh)
+    return log
