338db2a277d2e8f6f251a8941a5248fc1cec740f
Diego Neves <diego@diegoneves.eti.br>
2023-03-11 20:32:12 -0300

    solution

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,19 +1,21 @@
 import pytest
-
 from dundie.core import load
-
 from .constants import PEOPLE_FILE
+EXPECTED_NAMES = ["Jim Halpert", "Dwight Schrute", "Gabe Lewis"]
+EXPECTED_DEPTS = ["Sales", "Director"]
+EXPECTED_ROLES = ["Salesman", "Manager"]
+EMAIL_DOMAIN = "dundlermifflin.com"
 
 
 @pytest.mark.unit
 @pytest.mark.high
-def test_load_postive_has_3_people():
-    """Test load function."""
-    assert len(load(PEOPLE_FILE)) == 3
-
-
-@pytest.mark.unit
-@pytest.mark.high
-def test_load_postive_first_name_starts_with_j():
-    """Test load function."""
-    assert load(PEOPLE_FILE)[0][0] == "J"
+def test_load(request):
+    """Test loaded data conforms with loaded file"""
+    result = load(PEOPLE_FILE)  # load people.csv as a list of csv lines
+    assert len(result) == 3
+    for line in result:
+        data = line.split(", ")
+        assert data[0] in EXPECTED_NAMES
+        assert data[1] in EXPECTED_DEPTS
+        assert data[2] in EXPECTED_ROLES
+        assert data[3].split("@")[1] in EMAIL_DOMAIN
